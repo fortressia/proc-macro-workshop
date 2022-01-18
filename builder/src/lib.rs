51c49de90007f6385ae2a69095b69e54233c58bd
Nefo Fortressia <nefothingy@hotmail.com>
2022-01-18 19:08:02 +0700

    feat(builder): implement Step 03 to 05
    The following steps are finished in this commit:
    03 - Call Setters
    04 - Call build()
    05 - Method Chaining

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -9,42 +9,81 @@ pub fn derive(input: TokenStream) -> TokenStream {
     // let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
     let builder_ident = format_ident!("{}Builder", &ident);
     let mut properties = Vec::new();
-    let mut builder_init = Vec::new();
+    let mut setters = Vec::new();
+    let mut builder_setters = Vec::new();
+    let mut checkers = Vec::new();
     match input.data {
         Data::Struct(data) => match data.fields {
             syn::Fields::Named(fields) => {
                 for field in fields.named.into_iter() {
-                    let ident = field.ident.unwrap();
+                    let field_ident = field.ident.unwrap();
 
                     let the_type = field.ty;
                     properties.push(quote!(
-                        #ident: Option<#the_type>,
+                        #field_ident: Option<#the_type>,
                     ));
 
-                    builder_init.push(quote!(
-                        #ident: None,
+
+                    setters.push(quote!(
+                        pub fn #field_ident(&mut self, #field_ident: #the_type) -> &mut Self {
+                            self.#field_ident = Some(#field_ident);
+
+                            self
+                        }
                     ));
+                
+                    builder_setters.push(quote!(
+                        #field_ident: self.#field_ident.clone().unwrap(),
+                    ));
+
+                    checkers.push(quote!(
+                        if self.#field_ident.is_none() {
+                       
+                           return Err(Box::<dyn Error>::from("Property #field_ident is missing. Assign it by calling #builder_ident::#field_ident()"));
+                        }
+                    ))
                 }
             }
 
-            syn::Fields::Unnamed(_) | syn::Fields::Unit => unimplemented!(),
+            _ => unimplemented!(),
         },
         _ => unimplemented!(),
     };
 
-    quote!(
+    let output = quote!(
+        #[derive(Clone, Default)]
         pub struct #builder_ident {
-         #(#properties)*
+            #(#properties)*
+        }
+
+        impl #builder_ident {
+            #(#setters)*
+
+            pub fn new() -> Self {
+                Self::default()
+            }
+
+            pub fn build(&mut self) -> Result<#ident, Box<dyn std::error::Error>> {
+                use std::error::Error;
+          
+                #(#checkers)*
+
+                Ok(#ident {
+                    #(#builder_setters)*
+                })
+            }
         }
 
         impl #ident {
             pub fn builder() -> #builder_ident {
-                #builder_ident {
-                  #(#builder_init)*
-                }
+                #builder_ident::new()
             }
         }
 
-    )
-    .into()
+    );
+
+  
+
+    output.into()
+   
 }
